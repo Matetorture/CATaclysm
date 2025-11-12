5e83d7069463f5e71acc98bd6804a21432ab0fa3
Matetorture <przemek.pytlarz18@gmail.com>
2025-11-12 19:49:21 +0100

    change data

--- cataclysm/js/data/cardsData.js
+++ cataclysm/js/data/cardsData.js
@@ -7,6 +7,6 @@ export const cardsData = [
     new CatCard(4, '2.1', 'Plant Guardian', 'Forest', 'Epic', 9, 20000, 5, ['Plant'], 1),
     new CatCard(5, '2.2', 'Air Dancer', 'Sky', 'Legendary', 11, 20000, 7, ['Air'], 14),
     new CatCard(6, '2.3', 'Electric Spark', 'Thunder', 'Legendary', 13, 20000, 8, ['Electric'], 30),
-    new CatCard(7, '2.4', 'Frost Whisker', 'Frozen', 'Ultimate', 14, 20000, 6, ['Ice'], 62),
+    new CatCard(7, '2.4', 'Frost Whisker', 'Frozen', 'Ultimate', 14, 20000, 6, ['Ice', 'Holy'], 62),
     new CatCard(8, '2.5', 'Legendary Beast', 'Legendary', 'Legendary', 20, 20000, 10, ['Fire'], 5),
 ];
